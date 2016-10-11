5af4bf2ace2c0647c7a94cb70bb03d0274ad13e8
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-10-11 12:24:54 +0200

    added function to save to db

--- WebDatabase/Form1.cs
+++ WebDatabase/Form1.cs
@@ -12,6 +12,8 @@ namespace WebDatabase
 {
     public partial class Form1 : Form
     {
+
+        T4DBEntities MyDataBase = new T4DBEntities();
         public Form1()
         {
             InitializeComponent();
@@ -20,13 +22,34 @@ namespace WebDatabase
             //EF är en ORM = object relational mapper
             //EF kopplar ihop databastabellen med en klass i c# windowsprogrammet
 
-            T4DBEntities MyDataBase = new T4DBEntities();
+            
 
             foreach (var item in MyDataBase.Tables)
             {
                 listBox1.Items.Add(item);
             }
             listBox1.DisplayMember = "Headline";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Demo som lägger till ny artikel
+
+            string headline = "Demo headline";
+            string content = " OAJSDOJASOIJDSA";
+
+            Table MyTable = new Table() { Headline = headline, Content = content };
+
+            //Ny fin article(table) nu ska den in i DB
+            //Prata med EF som sköter kommunikation med DB
+
+            MyDataBase.Tables.Add(MyTable);
+
+            //table/artikel) tillagd bara i winforms, ej i DB
+            //Be EF att spara till BD
+            MyDataBase.SaveChanges();
+
+            MessageBox.Show("Data saved to DB");
         }
     }
 }
